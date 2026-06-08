f9ccdd7186b93a34604a7a228fc9f7498bf3c4c0
Reda <elansarimohamedreda01@gmail.com>
2026-06-08 10:00:00 +0200

    initialize spring boot jee

---
+++ src/main/java/ma/enset/elansari/mohamedreda/ElansariMohamedredaExamJeeApplication.java
@@ -0,0 +1,13 @@
+package ma.enset.elansari.mohamedreda;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ElansariMohamedredaExamJeeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ElansariMohamedredaExamJeeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ma/enset/elansari/mohamedreda/ElansariMohamedredaExamJeeApplicationTests.java
@@ -0,0 +1,13 @@
+package ma.enset.elansari.mohamedreda;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ElansariMohamedredaExamJeeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
